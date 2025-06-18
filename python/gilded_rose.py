f88d569d33ff1271acb19c4e1625f8fdd0be1a46
ashutosh <Ashutosh.Tiwari@united.com>
2025-06-18 22:22:22 +0530

    Refactor: Use Factory Pattern to simplify item update logic.
    - Moved item-type logic into separate classes (AgedBrieUpdater, ConjuredItemUpdater, etc.)
    - Created a factory to choose the right updater for each item.
    - GildedRose class now just delegates the update logic.
    - Makes the code easier to extend and maintain.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -8,86 +8,90 @@ class Item:
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
+
+# Base Updater and Utilities functionality
+class ItemUpdater:
+
+    def update(self, item):
+        raise NotImplementedError
+
+    def decrease_quality(self, item, amount=1):
+        item.quality = max(0, item.quality - amount)
+
+    def increase_quality(self, item, amount=1):
+        item.quality = min(50, item.quality + amount)
+
+
+class AgedBrieUpdater(ItemUpdater):
+    def update(self, item):
+        self.increase_quality(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self.increase_quality(item)
+
+
+class BackstagePassUpdater(ItemUpdater):
+    def update(self, item):
+        self.increase_quality(item)
+        if item.sell_in < 11:
+            self.increase_quality(item)
+        if item.sell_in < 6:
+            self.increase_quality(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class SulfurasUpdater(ItemUpdater):
+    def update(self, item):
+        pass
+
+
+class NormalItemUpdater(ItemUpdater):
+    def update(self, item):
+        self.decrease_quality(item)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self.decrease_quality(item)
+
+
+class ConjuredItemUpdater(ItemUpdater):
+    def update(self, item):
+        self.decrease_quality(item, 2)
+        item.sell_in -= 1
+        if item.sell_in < 0:
+            self.decrease_quality(item, 2)
+
+
+# Helper Methods
+
+# Factory class
+
+class ItemUpdaterFactory:
+    def get_updater(self, item):
+        name = item.name.lower()
+        if name == "Aged Brie":
+            return AgedBrieUpdater()
+        elif name.startswith("backstage passes"):
+            return BackstagePassUpdater()
+        elif name == "sulfuras, hand of ragnaros":
+            return SulfurasUpdater()
+        elif name.startswith("conjured"):
+            return ConjuredItemUpdater()
+        else:
+            return NormalItemUpdater()
+
+
 # -*- coding: utf-8 -*-
 
-
+# Main GildeRose Class
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
+        self.factory = ItemUpdaterFactory()
 
     def update_quality(self):
         for item in self.items:
-            if self.is_aged_brie(item):
-                self._update_aged_brie(item)
-            elif self.is_backstage_pass(item):
-                self._update_backstage_pass(item)
-            elif self.is_sulfuras(item):
-                self._update_sulfuras(item)
-            elif self.is_conjured(item):
-                self._update_conjured_item(item)
-            else:
-                self._update_normal_item(item)
-
-
-# Item-Type Update Methods
-
-def _update_aged_brie(self, item):
-    self.increase_quality(item)
-    item.sell_in -= 1
-    if item.sell_in < 0:
-        self.increase_quality(item)
-
-
-def _update_backstage_pass(self, item):
-    self.increase_quality(item)
-    if item.sell_in < 11:
-        self.increase_quality(item)
-    if item.sell_in < 6:
-        self.increase_quality(item)
-    item.sell_in -= 1
-    if item.sell_in < 0:
-        item.quality = 0
-
-
-def _update_sulfuras(self, item):
-    pass  # Legendary item, do nothing
-
-
-def _update_normal_item(self, item):
-    self.decrease_quality(item)
-    item.sell_in -= 1
-    if item.sell_in < 0:
-        self.decrease_quality(item)
-
-
-def _update_conjured_item(self, item):
-    self.decrease_quality(item, 2)
-    item.sell_in -= 1
-    if item.sell_in < 0:
-        self.decrease_quality(item, 2)
-
-
-# Helper Methods
-def is_aged_brie(self, item):
-    return item.name == "Aged Brie"
-
-
-def is_backstage_pass(self, item):
-    return item.name == "Backstage passes to a TAFKAL80ETC concert"
-
-
-def is_sulfuras(self, item):
-    return item.name == "Sulfuras, Hand of Ragnaros"
-
-
-def decrease_quality(self, item, amount=1):
-    item.quality = max(0, item.quality - amount)
-
-
-def increase_quality(self, item, amount=1):
-    item.quality = min(50, item.quality + amount)
-
-
-def is_conjured(self, item):
-    return item.name.lower().startswith("conjured")
+            updater = self.factory.get_updater(item)
+            updater.update(item)
