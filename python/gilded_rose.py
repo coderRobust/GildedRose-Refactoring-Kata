59a0c79f1c8cebcce059074495112d1b666c8bb4
ashutosh <Ashutosh.Tiwari@united.com>
2025-06-18 21:29:56 +0530

    Refactor: Simplified code using helper methods
    - Moved all repeated checks into helper methods like is_aged_brie(), is_backstage_pass(), and is_sulfuras().
    - Created increase_quality() and decrease_quality() to manage quality limits.
    - No change in code logic.
    - Makes the code easier to read.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -18,33 +18,33 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if not self.is_aged_brie(item) and not self.is_backstage_pass(item):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    if not self.is_sulfuras(item):
+                        self.decrease_quality(item)
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    self.increase_quality(item)
+                    if self.is_backstage_pass(item):
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            self.increase_quality(item)
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                            self.increase_quality(item)
+
+            if not self.is_sulfuras(item):
+                item.sell_in -= 1
+
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if not self.is_aged_brie(item):
+                    if not self.is_backstage_pass(item):
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                            if not self.is_sulfuras(item):
+                                self.decrease_quality(item)
                     else:
-                        item.quality = item.quality - item.quality
+                        item.quality = 0
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        self.increase_quality(item)
 
 
 def is_aged_brie(self, item):
